a79d8a440884d71bc8d8a94dbca6bdecbfc208a5
matheohero <134490234+matheohero@users.noreply.github.com>
2023-12-12 09:36:20 +0100

    update

--- NosValeur.php
+++ NosValeur.php
@@ -10,7 +10,11 @@
         <h1>Nos valeurs</h1>
     </div>
     <div>
-        - On vous garantie un séjour de qualiter
+        - On vous garantie un séjour de qualiter<br>
+        - A un prix abordable<br>
+        - On sera vous guider afin de vous permétre de passer un agréable sèjour<br>
+        - Notre restoration est l'une des meilleurs de la région avec 4.9/5 sur foodAdvisor<br>
+        - Plus de 95% de nos client sont satisfait et on vous promet que vous en feriez partie<br>
     </div>
 </body>
 </html>
