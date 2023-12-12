a1af4b65c6ba7127fd6e255722fa85400f29d9d8
matheohero <134490234+matheohero@users.noreply.github.com>
2023-12-12 09:28:26 +0100

    Add files via upload

---
+++ NosValeur.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <h1>Nos valeurs</h1>
+    </div>
+    <div>
+        - On vous garantie un séjour de qualiter
+    </div>
+</body>
+</html>
