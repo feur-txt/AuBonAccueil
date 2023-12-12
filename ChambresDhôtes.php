8db5925ca0bc8b0c0aea6ee03fd979f6d143eefe
matheohero <134490234+matheohero@users.noreply.github.com>
2023-12-12 10:06:31 +0100

    Add files via upload

---
+++ ChambresDhôtes.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chambres d'hôtes</title>
+</head>
+<body>
+        <div>
+            <h1>Nos chambre d'hôtes</h1>
+        </div>
+
+        <div>
+            <h2>1er Chambre</h2>
+            <p>Surface : 50 m²</p>
+            <p>Prix : 350 € / nuit</p>
+            <p>Nombre de lit : 2</p>
+            <img src="https://www.gites-de-france-gironde.com/img/liste/chambre-hotes-1615279251.jpg">
+            <p>Description : Chambre modeste pour 3 personne (1 lit double et 1 lit simple)</p>
+        </div>
+        <div>
+            <h2>2éme Chambre</h2>
+            <p>Surface : 200 m²</p>
+            <p>Prix : 849 € / nuit</p>
+            <p>Nombre de lit : 8</p>
+            <img src="https://www.gites-de-france-gironde.com/img/liste/chambre-hotes-1615279251.jpg">
+            <p>Description : Chambre spacieuse pour 13 personne (5 lit double et 3 lit simple)</p>
+        </div>
+        <div>
+            <h2>3iém Chambre</h2>
+            <p>Surface : 20 m²</p>
+            <p>Prix : 150 € / nuit</p>
+            <p>Nombre de lit : 1</p>
+            <img src="https://www.gites-de-france-gironde.com/img/liste/chambre-hotes-1615279251.jpg">
+            <p>Description : Petite chambre pour 2 personne (1 lit double)</p>
+        </div>
+        <div>
+            <h2>4iém Chambre</h2>
+            <p>Surface : 25 m²</p>
+            <p>Prix : 200 € / nuit</p>
+            <p>Nombre de lit : 2</p>
+            <img src="https://www.gites-de-france-gironde.com/img/liste/chambre-hotes-1615279251.jpg">
+            <p>Description : Petite chambre pour 3 personne (1 lit double et 1 lit simple)</p>
+        </div>
+</body>
+</html>
